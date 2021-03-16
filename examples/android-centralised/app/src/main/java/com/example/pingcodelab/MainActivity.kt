3ca685bed4bca1a0d9dfce0d65db785fd03b685a
Gus Narea <gnarea@users.noreply.github.com>
2021-03-16 16:46:47 +0000

    mostly implement main activity

--- examples/android-centralised/app/src/main/java/com/example/pingcodelab/MainActivity.kt
+++ examples/android-centralised/app/src/main/java/com/example/pingcodelab/MainActivity.kt
@@ -1,11 +1,71 @@
 package com.example.pingcodelab
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import java.util.Date
+import kotlinx.android.synthetic.main.activity_main.clear
+import kotlinx.android.synthetic.main.activity_main.pings
+import kotlinx.android.synthetic.main.activity_main.send
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import tech.relaycorp.awaladroid.GatewayClient
 
+@ExperimentalCoroutinesApi
 class MainActivity : AppCompatActivity() {
+    private val pingRepository by lazy { (applicationContext as App).pingRepository }
+
+    private val backgroundContext = lifecycleScope.coroutineContext + Dispatchers.IO
+    private val backgroundScope = CoroutineScope(backgroundContext)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycleScope.launch {
+            withContext(backgroundContext) {
+                GatewayClient.bind()
+            }
+            send.isEnabled = true
+        }
+
+        pingRepository
+            .observe()
+            .onEach {
+                pings.text = it.joinToString("\n") { message ->
+                    "Ping (sent=${Date(message.sent)}) (received=${
+                        message.received?.let {
+                            Date(message.received)
+                        }
+                    })"
+                }
+            }
+            .launchIn(lifecycleScope)
+
+        send.setOnClickListener {
+            backgroundScope.launch {
+                sendPing()
+            }
+        }
+
+        clear.setOnClickListener {
+            backgroundScope.launch {
+                pingRepository.clear()
+            }
+        }
+    }
+
+    private suspend fun sendPing() {
+        // TODO
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        GatewayClient.unbind()
     }
 }
