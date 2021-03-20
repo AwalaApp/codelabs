08fd74c4b83c9a3d6647484b656f9c21226c71ac
Gus Narea <gnarea@users.noreply.github.com>
2021-03-20 14:47:42 +0000

    dynamically discover routes

--- examples/nodejs-pong/src/server.ts
+++ examples/nodejs-pong/src/server.ts
@@ -1,14 +1,20 @@
 require('make-promises-safe');
 
 import { fastify } from 'fastify';
+import fs from 'fs'
+import { join } from 'path'
 
 const IS_DEV = process.env.IS_DEV === 'true';
 const PORT = process.env.PORT || 8080;
 
+const ROUTES_DIR = join(__dirname, 'routes');
+
 async function main(): Promise<void> {
     const server = fastify();
 
-    await server.register(import('./routes'));
+    const routes = await getAllRouteFiles();
+    await Promise.all(routes.map((r) => server.register(import(r))));
+
     await server.ready();
     await server.listen(PORT);
 
@@ -16,6 +22,12 @@ async function main(): Promise<void> {
         console.info(`The server is listening on http://127.0.0.1:${PORT}`);
         console.info('Press Ctrl+C to quit.');
     }
+}
+
+async function getAllRouteFiles() {
+    const allFiles = await fs.promises.readdir(ROUTES_DIR);
+    const routeFiles = allFiles.filter((f) => f.endsWith('.js'));
+    return routeFiles.map((fileName) => join(ROUTES_DIR, fileName));
 }
 
 main();
